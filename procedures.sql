e3e818ff9c7414aa55b59e8a048d3d4e1d0d882d
Cris Motinha <cristinamotinham@gmail.com>
2018-11-19 23:10:16 -0200

    Arruma tabs e spaces

--- procedures.sql
+++ procedures.sql
@@ -20,11 +20,11 @@ drop procedure IniciaVenda
 DELIMITER //
 CREATE PROCEDURE AdicionaNoCarrinho (IN id_produto INT, IN qtd_produto INT, IN id_carrinho INT)
 BEGIN
-	DECLARE preco_produto DECIMAL(10,2);
+    DECLARE preco_produto DECIMAL(10,2);
     SELECT preco INTO preco_produto FROM Produto p WHERE p.id = id_produto;
-	INSERT INTO Item (id_produto, qtd, sub_total) VALUES (id_produto, qtd_produto, preco_produto * qtd); 
+    INSERT INTO Item (id_produto, qtd, sub_total) VALUES (id_produto, qtd_produto, preco_produto * qtd); 
     INSERT INTO ItemCarrinho (id_item, id_carrinho) VALUES
-		((SELECT max(id) FROM Item i WHERE i.id_produto = id_produto AND i.qtd = qtd_produto), id_carrinho);
+        ((SELECT max(id) FROM Item i WHERE i.id_produto = id_produto AND i.qtd = qtd_produto), id_carrinho);
 END //
 DELIMITER ;
 
@@ -45,7 +45,7 @@ drop procedure FinalizaVenda
 DELIMITER //
 CREATE PROCEDURE FinalizaVenda (IN carrinho_id INT, IN id_tipo_pagamento INT, IN id_entrega INT, IN id_funcionario_caixa INT, OUT valor_final DECIMAL(10,2))
 BEGIN
-	DECLARE done INT DEFAULT 0;
+    DECLARE done INT DEFAULT 0;
     DECLARE cur_id_produto INT;
     DECLARE cur_tem_materia_prima BOOL;
     DECLARE cur_qtd_produto INT;
@@ -54,39 +54,39 @@ BEGIN
     
     DECLARE cursor_id_produtos cursor for select id_produto, Qtd, tem_materia_prima from ItensNoCarrinho;
     DECLARE continue handler for not found set done = 1;
-	
+    
     SELECT c.id_cliente into cliente FROM Carrinho c WHERE c.id = carrinho_id;
     
     DROP TABLE IF EXISTS ItensNoCarrinho;
     CREATE TEMPORARY TABLE ItensNoCarrinho
     SELECT i.id_produto, sum(i.qtd) as 'Qtd', sum(i.sub_total) as 'Soma', p.tem_materia_prima
     FROM ItemCarrinho ic
-		INNER JOIN Item i ON ic.id_item = i.id
+        INNER JOIN Item i ON ic.id_item = i.id
         INNER JOIN Produto p ON p.id = i.id_produto
-	WHERE ic.id_carrinho = carrinho_id
+    WHERE ic.id_carrinho = carrinho_id
     GROUP BY i.id_produto;
     
     SELECT sum('Soma') INTO valor_final FROM ItensNoCarrinho;
     
-	INSERT INTO Venda (id_carrinho, id_pgto, id_entrega, id_func, id_cliente, data_venda, valor_total) VALUES
+    INSERT INTO Venda (id_carrinho, id_pgto, id_entrega, id_func, id_cliente, data_venda, valor_total) VALUES
     (carrinho_id, id_tipo_pagamento, id_entrega, id_funcionario_caixa, cliente, curdate(), valor_final);
 
-	
+    
 
-	OPEN cursor_id_produtos;
+    OPEN cursor_id_produtos;
     REPEAT
-		FETCH cursor_id_produtos into cur_id_produto, cur_qtd_produto, cur_tem_materia_prima;
+        FETCH cursor_id_produtos into cur_id_produto, cur_qtd_produto, cur_tem_materia_prima;
         
         CASE cur_tem_materia_prima WHEN false THEN
-			UPDATE EstoqueProduto ep
+            UPDATE EstoqueProduto ep
             SET ep.qtd = ep.qtd - cur_qtd_produto
             WHERE ep.id_produto = cur_id_produto;
-		END CASE;
+        END CASE;
         
-	UNTIL done
-	END REPEAT;
+    UNTIL done
+    END REPEAT;
 
-	CLOSE cursor_id_produtos;
+    CLOSE cursor_id_produtos;
 
 END //
 DELIMITER ;
